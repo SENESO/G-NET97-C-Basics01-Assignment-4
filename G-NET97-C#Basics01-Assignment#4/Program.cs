9033752ef98406e399fc5d9233a9146b596df1bf
Eslam Ashraf <ElmistRo@outlook.com>
2026-07-29 22:45:46 +0300

    Add project files.

---
+++ G-NET97-C#Basics01-Assignment#4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET97_C_Basics01_Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
